5f8ec3ea7f56210e3ce258e9fab4679ce80ef167
Diogo Graciano <diogo.dg691@gmail.com>
2025-05-13 13:49:55 -0300

    Adicionado Endpoint do Checkout

---
+++ src/Checkout.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CodePhix\Asaas;
+
+
+class Checkout {
+
+    public $http;
+
+    public function __construct(Connection $connection)
+    {
+        $this->http = $connection;
+    }
+
+
+    public function create(array $dados){
+        return $this->http->post('/checkouts/', $dados);
+    }
+
+    public function cancel(string $id){
+        return $this->http->post('/checkouts/'.$id.'/cancel',[]);
+    }
+
+}
